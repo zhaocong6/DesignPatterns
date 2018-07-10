872ce37b4ec82d25264a00afdccbe8877f93c7d3
zhaocong6 <1140253608@qq.com>
2018-07-10 10:46:26 +0800

    单例模式

--- Singleton/Test/test.php
+++ Singleton/Test/test.php
@@ -6,8 +6,8 @@
  * Time: 下午6:08
  */
 
-include './Car.php';
-include './Exception/SingletonException.php';
+include '../Car.php';
+include '../Exception/SingletonException.php';
 
 try{
     $car1 = Car::instance();
